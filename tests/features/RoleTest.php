7910087b39e5f7637fe4b4f3e4257903710a0653
mishu <mishu@xtelecom.ro>
2017-08-13 00:44:24 +0300

    added viewIs asserts in tests. added icons in forms

--- tests/features/RoleTest.php
+++ tests/features/RoleTest.php
@@ -6,9 +6,9 @@ use Faker\Factory;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use LaravelEnso\MenuManager\app\Models\Menu;
 use LaravelEnso\RoleManager\app\Models\Role;
-use Tests\TestCase;
+use LaravelEnso\TestHelper\app\Classes\TestHelper;
 
-class RoleTest extends TestCase
+class RoleTest extends TestHelper
 {
     use DatabaseMigrations;
 
@@ -20,37 +20,38 @@ class RoleTest extends TestCase
 
         // $this->disableExceptionHandling();
         $this->faker = Factory::create();
-        $this->actingAs(User::first());
+        $this->signIn(User::first());
     }
 
     /** @test */
     public function index()
     {
-        $response = $this->get('/system/roles');
-
-        $response->assertStatus(200);
+        $this->get('/system/roles')
+            ->assertStatus(200)
+            ->assertViewIs('laravel-enso/rolemanager::index');
     }
 
     /** @test */
     public function create()
     {
-        $response = $this->get('/system/roles/create');
-
-        $response->assertStatus(200);
+        $this->get('/system/roles/create')
+            ->assertStatus(200)
+            ->assertViewIs('laravel-enso/rolemanager::create')
+            ->assertViewHas('form');
     }
 
     /** @test */
     public function store()
     {
         $postParams = $this->postParams();
-        $response = $this->post('/system/roles', $postParams);
+        $response   = $this->post('/system/roles', $postParams);
 
         $role = Role::whereName($postParams['name'])->first();
 
         $response->assertStatus(200)
             ->assertJsonFragment([
-            'message' => 'The role was created!',
-            'redirect'=> '/system/roles/'.$role->id.'/edit',
+                'message'  => 'The role was created!',
+                'redirect' => '/system/roles/' . $role->id . '/edit',
             ]);
     }
 
@@ -59,26 +60,26 @@ class RoleTest extends TestCase
     {
         $role = Role::first();
 
-        $response = $this->get('/system/roles/'.$role->id.'/edit');
+        $response = $this->get('/system/roles/' . $role->id . '/edit');
 
         $response->assertStatus(200)
             ->assertViewHas('role', $role)
+            ->assertViewIs('laravel-enso/rolemanager::edit')
             ->assertViewHas('form');
     }
 
     /** @test */
     public function update()
     {
-        $role = Role::create($this->postParams());
+        $role       = Role::create($this->postParams());
         $role->name = 'edited';
-        $data = $role->toArray();
-        $data['_method'] = 'PATCH';
+        $data       = $role->toArray();
 
-        $this->patch('/system/roles/'.$role->id, $data)
+        $this->patch('/system/roles/' . $role->id, $data)
             ->assertStatus(200)
             ->assertJson(['message' => __(config('labels.savedChanges'))]);
 
-        $this->assertTrue($this->wasUpdated($role));
+        $this->assertEquals('edited', Role::whereId($role->id)->first(['name'])->name);
     }
 
     /** @test */
@@ -86,52 +87,27 @@ class RoleTest extends TestCase
     {
         $role = Role::create($this->postParams());
 
-        $response = $this->delete('/system/roles/'.$role->id);
+        $this->delete('/system/roles/' . $role->id)
+            ->assertStatus(200)
+            ->assertJsonFragment(['message']);
 
-        $this->hasJsonConfirmation($response);
-        $this->wasDeleted($role);
-        $response->assertStatus(200);
+        $this->assertNull(Role::whereName($role->name)->first());
     }
 
     /** @test */
-    public function cantDestroyIfHasUsers()
+    public function cant_destroy_if_has_users()
     {
         $role = Role::create($this->postParams());
-        $this->addNewUser($role);
+        $this->createUser($role);
 
-        $response = $this->delete('/system/roles/'.$role->id);
+        $this->delete('/system/roles/' . $role->id)
+            ->assertStatus(302)
+            ->assertSessionHas('flash_notification');
 
-        $response->assertStatus(302);
-        $this->assertTrue($this->hasSessionErrorMessage());
-        $this->wasNotDeleted($role);
+        $this->assertNotNull(Role::whereId($role->id)->first());
     }
 
-    private function wasUpdated($role)
-    {
-        return Role::whereId($role->id)->first(['name'])->name === 'edited';
-    }
-
-    private function wasDeleted($role)
-    {
-        return $this->assertNull(Role::whereName($role->name)->first());
-    }
-
-    private function wasNotDeleted($role)
-    {
-        return $this->assertNotNull(Role::whereId($role->id)->first());
-    }
-
-    private function hasJsonConfirmation($response)
-    {
-        return $response->assertJsonFragment(['message']);
-    }
-
-    private function hasSessionErrorMessage()
-    {
-        return session('flash_notification')[0]->level === 'danger';
-    }
-
-    private function addNewUser($role)
+    private function createUser($role)
     {
         $user = new User([
             'first_name' => $this->faker->firstName,
@@ -139,20 +115,20 @@ class RoleTest extends TestCase
             'phone'      => $this->faker->phoneNumber,
             'is_active'  => 1,
         ]);
-        $user->email = $this->faker->email;
+        $user->email    = $this->faker->email;
         $user->owner_id = Owner::first(['id'])->id;
-        $user->role_id = $role->id;
+        $user->role_id  = $role->id;
         $user->save();
     }
 
     private function postParams()
     {
         return [
-             'name'                 => $this->faker->word,
-             'display_name'         => $this->faker->word,
-             'description'          => $this->faker->sentence,
-             'menu_id'              => Menu::first(['id'])->id,
-            '_method'               => 'POST',
+            'name'         => $this->faker->word,
+            'display_name' => $this->faker->word,
+            'description'  => $this->faker->sentence,
+            'menu_id'      => Menu::first(['id'])->id,
+            '_method'      => 'POST',
         ];
     }
 }
