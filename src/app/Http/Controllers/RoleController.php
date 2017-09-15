046e58aa197777b711892b64efa0f8c585573d05
Mihai <mihai.ocneanu@gmail.com>
2017-09-15 11:34:42 +0300

    cleanup of redundant imports and select/datatable traits, as that functionality is extracted to dedicated controllers

--- src/app/Http/Controllers/RoleController.php
+++ src/app/Http/Controllers/RoleController.php
@@ -4,30 +4,17 @@ namespace LaravelEnso\RoleManager\app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use LaravelEnso\DataTable\app\Traits\DataTable;
-use LaravelEnso\RoleManager\app\DataTable\RolesTableStructure;
 use LaravelEnso\RoleManager\app\Http\Requests\ValidateRoleRequest;
 use LaravelEnso\RoleManager\app\Http\Services\RoleService;
 use LaravelEnso\RoleManager\app\Models\Role;
-use LaravelEnso\Select\app\Traits\SelectListBuilder;
 
 class RoleController extends Controller
 {
-    use DataTable, SelectListBuilder;
-
     private $roles;
-
-    protected $selectSourceClass = Role::class;
-    protected $tableStructureClass = RolesTableStructure::class;
 
     public function __construct(Request $request)
     {
         $this->roles = new RoleService($request);
-    }
-
-    public function getTableQuery()
-    {
-        return $this->roles->getTableQuery();
     }
 
     public function index()
