96966c6b52956bbefcf9e19c2b6b7fc42323a4ea
Mihai <mihai.ocneanu@gmail.com>
2017-09-19 15:37:04 +0300

    fixed versions. Moved VueJS components to the vueadminlte package | added missing vueadminlte dependency | fixed composer.json provider classpath

--- src/RolesServiceProvider.php
+++ src/RolesServiceProvider.php
@@ -9,7 +9,6 @@ class RolesServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadDependencies();
-        $this->publishesAll();
     }
 
     private function loadDependencies()
@@ -17,17 +16,6 @@ class RolesServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-enso/rolemanager');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-    }
-
-    private function publishesAll()
-    {
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'roles-components');
-
-        $this->publishes([
-            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
-        ], 'enso-update');
     }
 
     public function register()
