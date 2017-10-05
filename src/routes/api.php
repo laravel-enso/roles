a805b76fd3e703af1ee0811c2684f76287593b53
mishu <mishu@xtelecom.ro>
2017-10-05 15:07:51 +0300

    refactor routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,29 +1,26 @@
 <?php
 
-Route::middleware(['auth:api', 'api', 'core'])
-    ->prefix('api')
+Route::middleware(['web', 'auth', 'core'])
+    ->prefix('api/system')->as('system.')
     ->namespace('LaravelEnso\RoleManager\app\Http\Controllers')
     ->group(function () {
-        Route::prefix('system')->as('system.')
+        Route::prefix('roles')->as('roles.')
             ->group(function () {
-                Route::prefix('roles')->as('roles.')
-                    ->group(function () {
-                        Route::get('initTable', 'RoleTableController@initTable')
-                            ->name('initTable');
-                        Route::get('getTableData', 'RoleTableController@getTableData')
-                            ->name('getTableData');
-                        Route::get('exportExcel', 'RoleTableController@exportExcel')
-                            ->name('exportExcel');
+                Route::get('initTable', 'RoleTableController@initTable')
+                    ->name('initTable');
+                Route::get('getTableData', 'RoleTableController@getTableData')
+                    ->name('getTableData');
+                Route::get('exportExcel', 'RoleTableController@exportExcel')
+                    ->name('exportExcel');
 
-                        Route::get('getOptionList', 'RoleSelectController@getOptionList')
-                            ->name('getOptionList');
+                Route::get('getOptionList', 'RoleSelectController@getOptionList')
+                    ->name('getOptionList');
 
-                        Route::get('getPermissions/{role}', 'RolePermissionController@index')
-                            ->name('getPermissions');
-                        Route::post('setPermissions', 'RolePermissionController@update')
-                            ->name('setPermissions');
-                    });
-
-                Route::resource('roles', 'RoleController', ['except' => ['show']]);
+                Route::get('getPermissions/{role}', 'RolePermissionController@index')
+                    ->name('getPermissions');
+                Route::post('setPermissions', 'RolePermissionController@update')
+                    ->name('setPermissions');
             });
+
+        Route::resource('roles', 'RoleController', ['except' => ['show']]);
     });
