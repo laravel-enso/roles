a6fb2a0c4d0dfa768ed713d4cadf91441232a891
mishu <mishu@xtelecom.ro>
2018-03-23 10:41:24 +0200

    updates composer.json

--- src/database/migrations/2017_01_01_126000_create_structure_for_roles.php
+++ src/database/migrations/2017_01_01_126000_create_structure_for_roles.php
@@ -18,7 +18,7 @@ class CreateStructureForRoles extends StructureMigration
         ['name' => 'system.roles.store', 'description' => 'Store newly created role', 'type' => 1, 'default' => false],
         ['name' => 'system.roles.update', 'description' => 'Update role', 'type' => 1, 'default' => false],
         ['name' => 'system.roles.destroy', 'description' => 'Delete role', 'type' => 1, 'default' => false],
-        ['name' => 'system.roles.selectOptions', 'description' => 'Get role permissions list for vue-select', 'type' => 0, 'default' => false],
+        ['name' => 'system.roles.selectOptions', 'description' => 'Get role permissions list for vue-select', 'type' => 0, 'default' => true],
         ['name' => 'system.roles.getPermissions', 'description' => 'Get role permissions for role configurator', 'type' => 0, 'default' => false],
         ['name' => 'system.roles.setPermissions', 'description' => 'Set role permissions for role configurator', 'type' => 1, 'default' => false],
     ];
