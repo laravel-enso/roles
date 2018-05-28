97c11eba84cc2a80d6e8b64c4c4f4fc8ac5c690e
mishu <mishu@xtelecom.ro>
2018-05-28 13:27:33 +0300

    updates for the new structure manager

--- src/database/migrations/2017_01_01_126000_create_structure_for_roles.php
+++ src/database/migrations/2017_01_01_126000_create_structure_for_roles.php
@@ -9,22 +9,22 @@ class CreateStructureForRoles extends StructureMigration
     ];
 
     protected $permissions = [
-        ['name' => 'system.roles.getTableData', 'description' => 'Get table data for roles', 'type' => 0, 'default' => false],
-        ['name' => 'system.roles.exportExcel', 'description' => 'Export excel for roles', 'type' => 0, 'default' => false],
-        ['name' => 'system.roles.initTable', 'description' => 'Init table for roles menu', 'type' => 0, 'default' => false],
-        ['name' => 'system.roles.create', 'description' => 'Create role', 'type' => 1, 'default' => false],
-        ['name' => 'system.roles.edit', 'description' => 'Edit role', 'type' => 1, 'default' => false],
-        ['name' => 'system.roles.index', 'description' => 'Show roles index', 'type' => 0, 'default' => false],
-        ['name' => 'system.roles.store', 'description' => 'Store newly created role', 'type' => 1, 'default' => false],
-        ['name' => 'system.roles.update', 'description' => 'Update role', 'type' => 1, 'default' => false],
-        ['name' => 'system.roles.destroy', 'description' => 'Delete role', 'type' => 1, 'default' => false],
-        ['name' => 'system.roles.selectOptions', 'description' => 'Get role permissions list for vue-select', 'type' => 0, 'default' => true],
-        ['name' => 'system.roles.getPermissions', 'description' => 'Get role permissions for role configurator', 'type' => 0, 'default' => false],
-        ['name' => 'system.roles.setPermissions', 'description' => 'Set role permissions for role configurator', 'type' => 1, 'default' => false],
+        ['name' => 'system.roles.getTableData', 'description' => 'Get table data for roles', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.roles.exportExcel', 'description' => 'Export excel for roles', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.roles.initTable', 'description' => 'Init table for roles menu', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.roles.create', 'description' => 'Create role', 'type' => 1, 'is_default' => false],
+        ['name' => 'system.roles.edit', 'description' => 'Edit role', 'type' => 1, 'is_default' => false],
+        ['name' => 'system.roles.index', 'description' => 'Show roles index', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.roles.store', 'description' => 'Store newly created role', 'type' => 1, 'is_default' => false],
+        ['name' => 'system.roles.update', 'description' => 'Update role', 'type' => 1, 'is_default' => false],
+        ['name' => 'system.roles.destroy', 'description' => 'Delete role', 'type' => 1, 'is_default' => false],
+        ['name' => 'system.roles.selectOptions', 'description' => 'Get role permissions list for vue-select', 'type' => 0, 'is_default' => true],
+        ['name' => 'system.roles.getPermissions', 'description' => 'Get role permissions for role configurator', 'type' => 0, 'is_default' => false],
+        ['name' => 'system.roles.setPermissions', 'description' => 'Set role permissions for role configurator', 'type' => 1, 'is_default' => false],
     ];
 
     protected $menu = [
-        'name' => 'Roles', 'icon' => 'universal-access', 'link' => 'system.roles.index', 'order' => 999, 'has_children' => false,
+        'name' => 'Roles', 'icon' => 'universal-access', 'link' => 'system.roles.index', 'order_index' => 999, 'has_children' => false,
     ];
 
     protected $parentMenu = 'System';
