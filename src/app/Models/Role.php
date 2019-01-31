82963928f7698d734df4c5a7728c2ef5672dcd72
mishu <mishu@xtelecom.ro>
2019-01-31 20:14:42 +0200

    adds table caching

--- src/app/Models/Role.php
+++ src/app/Models/Role.php
@@ -19,6 +19,8 @@ class Role extends Model
 
     protected $fillable = ['menu_id', 'name', 'display_name', 'description'];
 
+    protected $cachedTable = 'roles';
+
     public function menu()
     {
         return $this->belongsTo(Menu::class);
