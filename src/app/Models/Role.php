1d98ace5e66d26c14af5457a075967fbc5e09e2e
mishu <mishu@xtelecom.ro>
2019-02-02 11:53:03 +0200

    adds missing TableCache trait

--- src/app/Models/Role.php
+++ src/app/Models/Role.php
@@ -6,13 +6,14 @@ use LaravelEnso\Core\app\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use LaravelEnso\Core\app\Models\UserGroup;
 use LaravelEnso\MenuManager\app\Models\Menu;
+use LaravelEnso\VueDatatable\app\Traits\TableCache;
 use LaravelEnso\PermissionManager\app\Models\Permission;
 use LaravelEnso\Multitenancy\app\Traits\SystemConnection;
 use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 class Role extends Model
 {
-    use SystemConnection;
+    use SystemConnection, TableCache;
 
     const AdminId = 1;
     const SupervisorId = 2;
