0142838a72c2cc63be9f0253f62124c15193dc32
Mihai <mihai.ocneanu@gmail.com>
2019-12-13 18:05:11 +0200

    split routes

---
+++ src/routes/app/roles.php
@@ -0,0 +1,24 @@
+<?php
+
+Route::namespace('Role')
+    ->group(function () {
+        Route::get('create', 'Create')->name('create');
+        Route::post('', 'Store')->name('store');
+        Route::get('{role}/edit', 'Edit')->name('edit');
+        Route::patch('{role}', 'Update')->name('update');
+        Route::delete('{role}', 'Destroy')->name('destroy');
+
+        Route::get('initTable', 'InitTable')->name('initTable');
+        Route::get('tableData', 'TableData')->name('tableData');
+        Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+
+        Route::get('options', 'Options')->name('options');
+    });
+
+Route::namespace('Configure')
+    ->group(function () {
+        Route::get('getPermissions/{role}', 'Index')->name('getPermissions');
+        Route::post('setPermissions/{role}', 'Update')->name('setPermissions');
+    });
+
+Route::post('writeConfig/{role}', 'ConfigWriter')->name('writeConfig');
