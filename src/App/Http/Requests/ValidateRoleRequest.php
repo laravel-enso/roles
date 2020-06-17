2f65178977b9fef4aff1c59c9fa613b9596ecd9a
mishu <mishu@xtelecom.ro>
2020-06-17 08:29:16 +0300

    improves roles validation

--- src/App/Http/Requests/ValidateRoleRequest.php
+++ src/App/Http/Requests/ValidateRoleRequest.php
@@ -4,6 +4,7 @@ namespace LaravelEnso\Roles\App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use LaravelEnso\Menus\App\Models\Menu;
 
 class ValidateRoleRequest extends FormRequest
 {
@@ -22,9 +23,23 @@ class ValidateRoleRequest extends FormRequest
         ];
     }
 
+    public function withValidator($validator)
+    {
+        if ($this->chosenParentMenu()) {
+            $validator->after(fn ($validator) => $validator->errors()
+                ->add('menu_id', 'Parent menus cannot be set as role defaults'));
+        }
+    }
+
     protected function nameUnique()
     {
         return Rule::unique('roles', 'name')
             ->ignore(optional($this->route('role'))->id);
+    }
+
+    private function chosenParentMenu(): bool
+    {
+        return $this->filled('menu_id')
+            && Menu::find($this->get('menu_id'))->has_children;
     }
 }
