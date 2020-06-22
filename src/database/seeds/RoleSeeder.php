d7ec94a3e2c1ffab8f61c65e662fb559ac1cadb7
reza <raftx24@gmail.com>
2020-06-22 15:32:17 +0430

    adds api role

--- src/database/seeds/RoleSeeder.php
+++ src/database/seeds/RoleSeeder.php
@@ -10,6 +10,7 @@ class RoleSeeder extends Seeder
     private const Roles = [
         ['menu_id' => 1, 'name' => 'admin', 'display_name' => 'Administrator', 'description' => 'Administrator role. Full featured.'],
         ['menu_id' => 1, 'name' => 'supervisor', 'display_name' => 'Supervisor', 'description' => 'Supervisor role.'],
+        ['menu_id' => null, 'name' => 'api', 'display_name' => 'Api', 'description' => 'Api role.'],
     ];
 
     public function run()
@@ -17,11 +18,15 @@ class RoleSeeder extends Seeder
         $roles = (new Collection(self::Roles))
             ->map(fn ($role) => factory(Role::class)->create($role));
 
-        $admin = $roles->first();
+        $admin = $roles->where('name', 'admin')->first();
 
         $admin->permissions()->sync(Permission::pluck('id'));
 
-        $supervisor = $roles->last();
+        $api = $roles->where('name', 'api')->first();
+
+        $api->permissions()->sync(Permission::controlPanel()->pluck('id'));
+
+        $supervisor = $roles->where('name', 'supervisor')->first();
 
         $supervisor->permissions()->sync(Permission::implicit()->pluck('id'));
     }
