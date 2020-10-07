264e6084e2f875ce59e79f7f604c26e8237c4a1a
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:39:03 +0300

    updates routes

--- routes/app/roles.php
+++ routes/app/roles.php
@@ -1,15 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Roles\Http\Controllers\Create;
+use LaravelEnso\Roles\Http\Controllers\Destroy;
+use LaravelEnso\Roles\Http\Controllers\Edit;
+use LaravelEnso\Roles\Http\Controllers\ExportExcel;
+use LaravelEnso\Roles\Http\Controllers\InitTable;
+use LaravelEnso\Roles\Http\Controllers\Options;
+use LaravelEnso\Roles\Http\Controllers\Store;
+use LaravelEnso\Roles\Http\Controllers\TableData;
+use LaravelEnso\Roles\Http\Controllers\Update;
 
-Route::get('create', 'Create')->name('create');
-Route::post('', 'Store')->name('store');
-Route::get('{role}/edit', 'Edit')->name('edit');
-Route::patch('{role}', 'Update')->name('update');
-Route::delete('{role}', 'Destroy')->name('destroy');
+Route::get('create', Create::class)->name('create');
+Route::post('', Store::class)->name('store');
+Route::get('{role}/edit', Edit::class)->name('edit');
+Route::patch('{role}', Update::class)->name('update');
+Route::delete('{role}', Destroy::class)->name('destroy');
 
-Route::get('initTable', 'InitTable')->name('initTable');
-Route::get('tableData', 'TableData')->name('tableData');
-Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+Route::get('initTable', InitTable::class)->name('initTable');
+Route::get('tableData', TableData::class)->name('tableData');
+Route::get('exportExcel', ExportExcel::class)->name('exportExcel');
 
-Route::get('options', 'Options')->name('options');
+Route::get('options', Options::class)->name('options');
