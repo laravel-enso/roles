61ad967afd19d33dfb2dd24897cf3b2ff8e5a643
ALIN <=>
2025-04-16 13:30:34 +0300

    Adjusted enum frontend

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -3,11 +3,8 @@
 namespace LaravelEnso\Roles;
 
 use LaravelEnso\Enums\EnumServiceProvider as ServiceProvider;
-use LaravelEnso\Roles\Enums\Role;
 
 class EnumServiceProvider extends ServiceProvider
 {
-    public $register = [
-        'roles' => Role::class,
-    ];
+    public $register = [];
 }
--- src/Enums/Role.php
+++ src/Enums/Role.php
@@ -2,8 +2,15 @@
 
 namespace LaravelEnso\Roles\Enums;
 
-enum Role: int
+use LaravelEnso\Enums\Contracts\Frontend;
+
+enum Role: int implements Frontend
 {
     case Admin = 1;
     case Supervisor = 2;
+
+    public static function registerBy(): string
+    {
+        return 'roles';
+    }
 }
